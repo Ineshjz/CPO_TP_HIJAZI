f329e01db42663f01a0ff025c3ff937ecf02174d
hp <hp@DESKTOP-S26CBA5>
2023-09-26 17:14:52 +0200

    TP1_convertisseur_HIJAZI

---
+++ TP1_convertisseur_HIJAZI/src/tp1_convertisseur_hijazi/TP1_convertisseur_HIJAZI.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp1_convertisseur_hijazi;
+import java.util.Scanner;
+
+/**
+ *
+ * @author hp
+ */
+public class TP1_convertisseur_HIJAZI {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double reel;
+        System.out.println(" Saisir un reel");
+        reel = sc.nextDouble();
+        
+    }
+    
+}
