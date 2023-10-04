5dcccfb55481fcd379fa83f5e90d20232e5e1e00
hp <hp@DESKTOP-S26CBA5>
2023-10-04 18:04:52 +0200

    TP1_guessMyNumber_HIJAZI

---
+++ TP1_guessMyNumber_HIJAZI/src/tp1_guessmynumber_hijazi/TP1_guessMyNumber_HIJAZI.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp1_guessmynumber_hijazi;
+import java.util.Random;
+import java.util.Scanner;
+/**
+ *
+ * @author hp
+ */
+public class TP1_guessMyNumber_HIJAZI {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner sc;
+        Random generateurAleat = new Random();
+        System.out.println(generateurAleat);
+    }
+    
+}
