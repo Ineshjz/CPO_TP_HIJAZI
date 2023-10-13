3701318339201a2b40ad3fe1e5492affbc570841
inesh <inesh@DESKTOP-JL9QQ0S.education.lakanal.epf.fr>
2023-10-13 13:03:35 +0200

    TP2_convertisseurObjet_HIJAZI

---
+++ TP2_convertisseurObjet_HIJAZI/src/main/java/Convertisseur.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author inesh
+ */
+public class Convertisseur {
+        public int nbConversions; 
+    
+@Override
+public String toString () {
+ return "nb de conversions"+ nbConversions;
+}
+}
+
