1352ecdec77e3a99d0de45b8aac64b49bc82df01
inesh <inesh@DESKTOP-JL9QQ0S.education.lakanal.epf.fr>
2023-10-13 15:42:04 +0200

    TP2_convertisseurObjet_HIJAZI

--- TP2_convertisseurObjet_HIJAZI/src/main/java/com/mycompany/tp2_convertisseurobjet_hijazi/TP2_convertisseurObjet_HIJAZI.java
+++ TP2_convertisseurObjet_HIJAZI/src/main/java/com/mycompany/tp2_convertisseurobjet_hijazi/TP2_convertisseurObjet_HIJAZI.java
@@ -5,6 +5,8 @@
 
 package com.mycompany.tp2_convertisseurobjet_hijazi;
 
+import java.util.Scanner;
+
 /**
  *
  * @author inesh
@@ -12,6 +14,36 @@ package com.mycompany.tp2_convertisseurobjet_hijazi;
 public class TP2_convertisseurObjet_HIJAZI {
 
     public static void main(String[] args) {
-        
+Scanner sc = new Scanner(System.in);
+        double r= sc.nextDouble();
+        System.out.println(" Saisir un reel");
+        int x; 
+        System.out.println("""
+                           Saisissez la conversion que vous souhaiter effectuer :
+                           1) De Celcius vers Kelvin
+                           2) De Kelvin vers Celcius
+                           3) De Celsius vers Farenheit
+                           4) De Kelvin vers Farenheit
+                           6) De Farenheit vers Kelvin """);
+        Convertisseur c = new Convertisseur(); 
+      
+        x = sc.nextInt();
+        if (x ==1){
+            System.out.println("La valeur en Kelvin est " + c.CelciusVersKelvin(r) + c.toString());  
+        }
+        if (x ==2){
+            System.out.println("La valeur en Celsius est " + c.KelvinVersFarenheit(r)+ c.toString() );  
+        }
+        if (x ==3){
+            System.out.println("La valeur en Farenheit est " + c.CelciusVersFarenheit(r)+ c.toString() );  
+        }
+        if (x==4){
+            System.out.println("La valeur en Farenheit est " + c.KelvinVersFarenheit(r)+ c.toString() );  
+        }
+        if (x==5){
+            System.out.println("La valeur en Kelvin est " + c.FarenheitVersKelvin(r)+ c.toString() );  
+        }
     }
 }
+
+
