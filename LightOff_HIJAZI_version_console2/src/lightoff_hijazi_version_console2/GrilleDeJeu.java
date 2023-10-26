b75d33b006441cda0fcfea3b634bc3ab08479189
inesh <inesh@DESKTOP-JL9QQ0S.education.lakanal.epf.fr>
2023-10-26 21:03:46 +0200

    LightOff_HIJAZI_version_console

--- LightOff_HIJAZI_version_console2/src/lightoff_hijazi_version_console2/GrilleDeJeu.java
+++ LightOff_HIJAZI_version_console2/src/lightoff_hijazi_version_console2/GrilleDeJeu.java
@@ -40,29 +40,92 @@ public class GrilleDeJeu {
     }
     public void activerLigneColonneOuDiagonaleAleatoire(){
         Random rd = new Random(); 
-        int x= rd.nextInt(4);
-        if (x==0){ //active ligne aléatoire
+        int l= rd.nextInt(3);
+        if (l==0){ //active ligne aléatoire
             int x = rd.nextInt(nbLignes);
-            matriceCellules[nbLignes][nbColonnes].activerCellule(); 
+            for(int i=0; i<nbColonnes; i++){
+                matriceCellules[x][i].activerCellule();
+            }   
         }
-        if (x==1){ //active colonne aléatoire
+        if (l==1){ //active colonne aléatoire
             int x = rd.nextInt(nbColonnes);
-            matriceCellules[nbLignes][nbColonnes].activerCellule();
+            for(int i=0; i<nbLignes; i++){
+            matriceCellules[i][x].activerCellule();
         }
-        if (x==2){ //active diagonale aléatoire
+        }
+        if (l==2){ //active diagonale aléatoire
             for (int i=0; i<nbLignes; i++){
-                for (int j=0; j<nbColonnes; j++)
-            }
-            int x = rd.nextInt(nb);
-            matriceCellules[nbLignes][nbColonnes].activerCellule();
+            matriceCellules[i][i].activerCellule();
         }
-   
-         
-     }   
-        
-    
-    
-    
-           
-    
+        }
+        if (l==3){ 
+            for (int i=0; i<nbLignes; i++){
+            matriceCellules[i][(nbLignes-i)].activerCellule();
+            }
+        }
+        return matriceCellules; 
+        }
+    public void melangerMatriceAleatoirement (int nbTours){
+        this.eteindreToutesLesCellules(); 
+        for (int i =0; i<nbTours; i++){
+            this.activerLigneColonneOuDiagonaleAleatoire();
+        }
+    }
+    public void activerColonneDeCellules (int idColonne){
+        for (int i=0; i<nbLignes; i++){
+            matriceCellules[i][idColonne].activerCellule();
+        }
+    }
+    public void activerdiagonaleDescendante(){
+         for (int i=0; i<nbLignes; i++){
+             matriceCellules[i][i].activerCellule();
+         }
+    }
+    public boolean cellulesToutesEteintes(){
+        int n=0; 
+        boolean x = false; 
+        for (int i=0; i<nbLignes; i++){
+            for (int j=0; j<nbColonnes; j++){
+                if (matriceCellules[i][j].estEteint()==false){
+                    n+=1; 
+                }
+                else{
+                    n=n; 
+                }
+            }
+        }
+        if ( n==0){
+            x = true;
+    }
+        if (n>0){
+            x = false;
+        }
+        return x;
+    }
+
+    @Override
+    public String toString() { 
+        String a="";
+        String b=""; 
+        String c="";
+        for (int j=0; j<nbColonnes; j++){
+            a += j+" | ";
+        }
+        for (int j=0; j<=nbColonnes; j++){
+                c+="----";
+            }
+        for (int i=0; i<nbLignes; i++){
+            b += "\n"+c+"\n "+i+" | ";
+            for (int j=0; j<nbColonnes; j++){
+                b += this.matriceCellules[i][j]+" | ";
+            }
+        }
+        return "   | "+a+b;
+    }
 }
+
+    
+    }
+        
+       
+        
