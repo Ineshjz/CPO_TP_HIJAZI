8a32e5014707275bcb346ce8f4574c0f4fbcb53e
inesh <inesh@DESKTOP-JL9QQ0S.education.lakanal.epf.fr>
2023-11-01 16:50:07 +0100

    LightOff_HIJAZI_version_console

--- LightOff_HIJAZI_version_console2/src/lightoff_hijazi_version_console2/LightOff_HIJAZI_version_console2.java
+++ LightOff_HIJAZI_version_console2/src/lightoff_hijazi_version_console2/LightOff_HIJAZI_version_console2.java
@@ -14,18 +14,53 @@ public class LightOff_HIJAZI_version_console2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        CelluleLumineuse Noire = new CelluleLumineuse (false);
-        CelluleLumineuse Blanche = new CelluleLumineuse (true); 
-        Noire.activerCellule();
-        Blanche.activerCellule();
-        Noire.estEteint();
-        Blanche.estEteint(); 
-        Noire.eteindreCellule();
-        Blanche.eteindreCellule(); 
-        Noire.getEtat();
-        Blanche.getEtat();
+       /// CelluleLumineuse Noire = new CelluleLumineuse ();
+        //CelluleLumineuse Blanche = new CelluleLumineuse (); 
+        //Noire.activerCellule();
+        //Blanche.activerCellule();
+        //Noire.estEteint();
+        //Blanche.estEteint(); 
+        //Noire.eteindreCellule();
+        //Blanche.eteindreCellule(); 
+        //Noire.getEtat();
+        //Blanche.getEtat();
         
-        System.out.println("La classe CelluleLumineuse a bien été ajoutée");
+        //System.out.println("La classe CelluleLumineuse a bien été ajoutée");
+        
+        GrilleDeJeu affichage = new GrilleDeJeu(4,4);
+        affichage.cellulesToutesEteintes();
+        System.out.println(affichage);
+        
+        //affichage.activerColonneDeCellules(0);
+        //System.out.println(affichage);
+        //ok
+        
+        //affichage.activerLigneDeCellules(1);
+        //System.out.println(affichage);
+        //ok
+        
+        //affichage.activerdiagonaleDescendante();
+        //System.out.println(affichage);
+        //ok
+        
+        //affichage.activerdiagonaleMontante();
+        //System.out.println(affichage);
+        //Ne fonctionne pas
+        
+        //affichage.activerLigneColonneOuDiagonaleAleatoire();
+        //System.out.println(affichage);
+        // ok pour ligne et diagonale
+        
+        //affichage.melangerMatriceAleatoirement(2);
+        //System.out.println(affichage);
+        //ok 
+        
+        System.out.println("La classe GrilleDeJeu a été ajoutée etb fonctionne");
+
+        
+        
+        
+        
         
     }
     
